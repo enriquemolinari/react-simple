0926b2432f3cd4801d29fb187044b99bad9bc7cf
Enrique <enrique.molinari@gmail.com>
2018-07-17 15:43:48 -0300

    adding missing alt for img

--- src/UserDetail.js
+++ src/UserDetail.js
@@ -6,7 +6,7 @@ class UserDetail extends Component {
     let details = this.props.details;
     return (
       <div>
-        <p><img src={details.picture.thumbnail} /></p>
+        <p><img src={details.picture.thumbnail} alt="user fake face"/></p>
         <p><b>First Name: </b>{details.name.first}</p>
         <p><b>Last Name: </b>{details.name.last}</p>
         <p><b>Id: </b>{details.id.name} {details.id.value}</p>
